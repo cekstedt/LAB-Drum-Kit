5edbfd72c8837e99d322595022fceeaf80f3e18e
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-07-14 16:50:51 -0700

    Linter corrections.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function playSound(key) {
-  const SOUND_BINDINGS = {"w": "sounds/crash.mp3",
-                          "a": "sounds/kick-bass.mp3",
-                          "s": "sounds/snare.mp3",
-                          "d": "sounds/tom-1.mp3",
-                          "j": "sounds/tom-2.mp3",
-                          "k": "sounds/tom-3.mp3",
-                          "l": "sounds/tom-4.mp3"}
+  const SOUND_BINDINGS = {
+    w: "sounds/crash.mp3",
+    a: "sounds/kick-bass.mp3",
+    s: "sounds/snare.mp3",
+    d: "sounds/tom-1.mp3",
+    j: "sounds/tom-2.mp3",
+    k: "sounds/tom-3.mp3",
+    l: "sounds/tom-4.mp3"
+  };
   let audio = new Audio(SOUND_BINDINGS[key]);
   audio.play();
 }
